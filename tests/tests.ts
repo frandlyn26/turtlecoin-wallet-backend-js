2f84a70772d1ed91681a814078ef1b6f332c9057
Ivan Tse <github@nerdtower.com>
2021-05-13 15:41:35 -0700

    fixing swapNode and daemon events tests using public.turtlenode.net instead of fastpool.xyz

--- tests/tests.ts
+++ tests/tests.ts
@@ -25,7 +25,7 @@ import { ValidateParametersTests } from './lib/ValidateParametersTests'
 
 const doPerformanceTests: boolean = process.argv.includes('--do-performance-tests');
 
-const daemonAddress = 'fastpool.xyz';
+const daemonAddress = 'public.turtlenode.net';
 const daemonPort = 11898;
 
 function delay(ms: number): Promise<void> {
